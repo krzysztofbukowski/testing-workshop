d24b3db72dbe12dbe2ae19d71df2b32df22357b1
Krzysztof Bukowski <ja@krzysztofbukowski.pl>
2019-02-21 19:52:23 +0100

    fixes

--- other/configuration/calculator.solution/.babelrc.js
+++ other/configuration/calculator.solution/.babelrc.js
@@ -1,10 +1,11 @@
-const isTest = String(process.env.NODE_ENV) === 'test'
 module.exports = {
-  presets: [['env', {modules: isTest ? 'commonjs' : false}], 'react'],
+  presets: [
+    'env', 
+    'babel-preset-react'
+  ],
   plugins: [
     'syntax-dynamic-import',
     'transform-class-properties',
-    'transform-object-rest-spread',
-    isTest ? 'dynamic-import-node' : null,
-  ].filter(Boolean),
+    'transform-object-rest-spread'
+  ],
 }
